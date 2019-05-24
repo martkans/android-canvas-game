425eea3fb232a83288d2e19291f969fe5d82fa6c
Martin Kansik <martkans@outlook.com>
2019-05-25 00:44:14 +0200

    [master-feat] convert layout to landscape and add nyan cat moving thanks to acc

--- app/src/main/java/com/martkans/game/FullscreenActivity.kt
+++ app/src/main/java/com/martkans/game/FullscreenActivity.kt
@@ -6,13 +6,26 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
-import android.util.Log
+import android.util.DisplayMetrics
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.dynamicanimation.animation.DynamicAnimation
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
+import kotlinx.android.synthetic.main.activity_fullscreen.*
 
 
 class FullscreenActivity : AppCompatActivity(), SensorEventListener {
 
-    lateinit var sensorManager: SensorManager
+    companion object {
+        private const val SPEED_RATIO = 5
+        private const val LX_TRESHOLD = 50
+    }
+
+    private var currentY = 0f
+    private var heightScreen = 0f
+    private lateinit var sensorManager: SensorManager
+    private lateinit var animationNyanCat: SpringAnimation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +33,18 @@ class FullscreenActivity : AppCompatActivity(), SensorEventListener {
         setContentView(R.layout.activity_fullscreen)
         supportActionBar?.hide()
 
+        val display = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(display)
+        val density = resources.displayMetrics.density
+        heightScreen = display.heightPixels / density
+
+
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
         registerSensors()
+
+        animationNyanCat = SpringAnimation(findViewById<View>(R.id.catIV), DynamicAnimation.TRANSLATION_Y, currentY)
+
     }
 
     private fun registerSensors() {
@@ -39,13 +61,44 @@ class FullscreenActivity : AppCompatActivity(), SensorEventListener {
         )
     }
 
+    private fun changeBackgroundImage(lx: Float) {
+        if (lx < LX_TRESHOLD)
+            mainCL.background = getDrawable(R.drawable.night_scene)
+        else
+            mainCL.background = getDrawable(R.drawable.day_scene)
+    }
+
+    private fun calculateCurrentY(currentYModifier: Float) {
+
+        when {
+            currentY + currentYModifier < -heightScreen -> currentY = -heightScreen
+            currentY + currentYModifier > heightScreen -> currentY = heightScreen
+            else -> currentY += currentYModifier
+        }
+    }
+
+    private fun moveNyanCat() {
+
+        val forceY = SpringForce(currentY)
+            .setDampingRatio(SpringForce.DAMPING_RATIO_NO_BOUNCY)
+            .setStiffness(SpringForce.STIFFNESS_MEDIUM)
+
+        animationNyanCat.spring = forceY
+        animationNyanCat.start()
+
+    }
+
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
 
     override fun onSensorChanged(event: SensorEvent?) {
 
-        if (event!!.sensor.type == Sensor.TYPE_LIGHT)
-            Log.wtf("light", event.values[0].toString())
-        else
-            Log.wtf("acc", event.values.joinToString(" "))
+        if (event != null) {
+            if (event.sensor.type == Sensor.TYPE_LIGHT)
+                changeBackgroundImage(event.values[0])
+            else {
+                calculateCurrentY(event.values[1] * SPEED_RATIO)
+                moveNyanCat()
+            }
+        }
     }
 }
